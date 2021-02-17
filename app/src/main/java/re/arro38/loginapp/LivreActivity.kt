4484d9831b6e768566f1de451055785d58e1bb90
EtienneV <etienne.vaytilingom@gmail.com>
2021-02-17 13:20:26 +0400

    TODO Requete GET

--- app/src/main/java/re/arro38/loginapp/LivreActivity.kt
+++ app/src/main/java/re/arro38/loginapp/LivreActivity.kt
@@ -2,10 +2,23 @@ package re.arro38.loginapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LivreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_livre)
+
+        val etIdLivre = findViewById<EditText>(R.id.etIdLivre)
+        val btGetLivre = findViewById<Button>(R.id.btGetLivre)
+        val tvAuteur = findViewById<TextView>(R.id.tvAuteur)
+        val tvTitre = findViewById<TextView>(R.id.tvTitre)
+
+        btGetLivre.setOnClickListener {
+            val IdLivre = etIdLivre.text
+            //TODO REQUETE GET https://module5.etienne-vaytilingom.re/Livres/ IdLivre => Mettre le résultat dans tvAuteur.text et tvTitre.text
+        }
     }
 }
